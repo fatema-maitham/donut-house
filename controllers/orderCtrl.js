dfe14b309b00b2cee2465d736a9613ddfe09ace0
fatema-maitham <fatemamaitham1@gmail.com>
2026-08-23 14:03:57 +0300

    checkout initial misisng successful msg

--- controllers/orderCtrl.js
+++ controllers/orderCtrl.js
@@ -1,4 +1,6 @@
 const Order = require('../models/Order');
+const Donut = require('../models/donut');
+const Location = require('../models/Location');
 
 // GET user's orders
 const index = async (req, res) => {
@@ -32,7 +34,66 @@ const show = async (req, res) => {
     }
 };
 
+// POST one order
+const create = async (req, res) => {
+    try {
+        const user = req.session.user;
+        const location = req.body.location;
+
+        if (!user.cart || user.cart.length === 0) {
+            return res.send('Your cart is empty');
+        }
+
+        const items = [];
+
+        for (const cartItem of user.cart) {
+            const donut = await Donut.findById(cartItem.donutId);
+
+            items.push({
+                donut: donut._id,
+                quantity: cartItem.quantity,
+                price: donut.price,
+            });
+        }
+
+        const total = items.reduce(
+            (sum, item) => sum + item.price * item.quantity,
+            0
+        );
+
+        const order = await Order.create({
+            user: user._id,
+            location,
+            items,
+            total,
+        });
+
+        user.cart = [];
+        await user.save();
+
+        res.redirect(`/orders/${order._id}`);
+    } catch (err) {
+        console.log(err);
+        res.send('Something went wrong');
+    }
+};
+
+const newOrder = async (req, res) => {
+    try {
+        const locations = await Location.find({ available: true });
+
+        res.render('orders/new.ejs', {
+            locations,
+        });
+    } catch (err) {
+        console.log(err);
+        res.send('Something went wrong');
+    }
+};
+
 module.exports = {
     index,
     show,
+    new: newOrder,
+    create,
 };
--- routes/orderRouter.js
+++ routes/orderRouter.js
@@ -6,6 +6,10 @@ const orderCtrl = require('../controllers/orderCtrl');
 
 router.get('/', orderCtrl.index);
 
+router.get('/new', orderCtrl.new);
+
 router.get('/:id', orderCtrl.show);
+
+router.post('/', orderCtrl.create);
 
 module.exports = router;
