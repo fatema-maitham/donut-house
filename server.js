b178f8a852213923457802c6a7ab79ccff071b99
fatema-maitham <fatemamaitham1@gmail.com>
2026-08-27 17:47:35 +0300

    last fix

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ const path = require('path');
 const express = require('express');
 
 const app = express();
+app.use(express.static(path.join(__dirname, 'public')));
 
 const isProduction = process.env.NODE_ENV === 'production';
 
